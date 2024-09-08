56faaa7c266da90370a771932ff0beb0264b3882
fredericAS123 <email@frederic.com>
2024-09-08 13:03:09 +0800

    The serial port file is used by itself to send related instructions to the MCU. In this project, the instructions are printed out with print

--- OO/uart.py
+++ OO/uart.py
@@ -1,51 +1,29 @@
 import serial
 import threading
 import time
-from restart import *
-f = open('run.txt','a+')
+
+
 class MyUart:
     def __init__(self, baudrate, port="/dev/ttyUSB0"):
-        self.baud = baudrate
-        self.port = port
         self.ser = serial.Serial(port, baudrate)
-        # while self.ser.is_open() != True:
-        
-        #     self.ser = serial.Serial(port, baudrate)
-        
         self.read_thread = threading.Thread(target=self.read_thread)
         self.read_thread.start()
-        self.mode = 4
+        self.msg = "none"
 
     def write(self, data):
-        if self.ser.isOpen():
-            try:
-                self.ser.write(data.encode("utf-8"))
-                #f.write(f"{time.time()}+{data}\n")
-            except:
-                time.sleep(5)
-                self.ser.close()
-                time.sleep(1)
-                self.ser = serial.Serial(self.port, self.baud)
-        else:
-            self.ser = serial.Serial(self.port, self.baud)
+        self.ser.write(data.encode("utf-8"))
         # time.sleep(0.1)
 
     def read_thread(self):
         while True:
-            rx_buf = self.ser.read(1).hex()
-            if rx_buf:
-                if int(rx_buf) != 9:
-                    self.mode = int(rx_buf)
-                    print("uart",rx_buf)
-                else:
-                    restart()
-        # while True:
-        #     time.sleep(0.02)
-        #     if self.ser.inWaiting() >= 1:
-        #         rx_buf = self.ser.read(1).hex()
-        #         self.mode = int(rx_buf)
-        #         print("uart rx",self.mode)
-                
+            time.sleep(0.002)
+
+            num = self.ser.inWaiting()
+            if num:
+                self.msg = self.ser.read(num)
+            else:
+                self.msg = "none"
+
     def close(self):
         self.ser.close()
 
@@ -55,11 +33,12 @@ class MyUart:
 
 
 if __name__ == '__main__':
-    uart = MyUart(115200, "/dev/ttyUSB0")
+    uart = MyUart(115200, "COM7")
     while True:
-        #uart.write("hello")
-        #print(uart.mode)
-        pass
-        #time.sleep(1)
+        uart.write("hello")
+        print(uart.msg)
+        time.sleep(1)
     uart.close()
+
+
     print("uart is closed")
