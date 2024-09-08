5c2ae7c7edc07ec9598ff8540307866af19e6b28
fredericAS123 <email@frederic.com>
2024-09-08 11:16:30 +0800

    Updated part of yolov8 detection

--- yolo.py
+++ yolo.py
@@ -7,11 +7,9 @@ import numpy as np
 model = YOLO_(r"D:\yolov8\ultralytics-main\ultralytics-main\ultralytics\runs\detect\train12\weights\best.pt")
 
 class turn_cam(camera.Camera):
-    def __init__(self, cap1, cap2, frame1, array0):
+    def __init__(self,  frame1, array0):
         super().__init__()
-        self.cam1=cap1
-        self.cam2=cap2
-        self.frame=frame1
+        self.frame = frame1
         self.array = array0
     def yolo_detect(self):
         results = model(self.frame, show=True)
@@ -26,10 +24,10 @@ class turn_cam(camera.Camera):
                     self.array.extend(r.boxes)
         print(len(self.array))
         if (len(self.array)) > 3:
-            self.cam2.stop()
+            self.stop()
             print("Device 2 closed")
             time.sleep(2)
-            self.cam2.restart()
+            self.restart()
             print("Device 2 opened")
 
 
